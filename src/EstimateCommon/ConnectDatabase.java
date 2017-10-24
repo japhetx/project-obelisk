4b29b8df6bf8cd8f4278a966e94e008c32458582
Japhet Mert Obsioma <jpmrobsioma@gmail.com>
2017-10-25 03:31:37 +0800

    Successfully separate ConnectionDatabase

---
+++ src/EstimateCommon/ConnectDatabase.java
@@ -0,0 +1,26 @@
+package EstimateCommon;
+
+import java.sql.*;
+
+public class ConnectDatabase {
+	
+	private static Connection conn;
+	
+	 public static Connection getConnection() {
+	   try {
+		Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	   String url = "jdbc:mysql://localhost:3306/systemdb1";
+	   String name = "root";
+	   String password = "";
+	   
+	   try {
+		   conn = DriverManager.getConnection(url, name, password);
+	   } catch (SQLException e) {
+		   e.printStackTrace();
+	   }
+	   return conn;
+	 }
+}
--- src/EstimateCommon/EstimateLogIn.java
+++ src/EstimateCommon/EstimateLogIn.java
@@ -3,54 +3,34 @@ package EstimateCommon;
 import java.sql.*;
 import java.util.Scanner;
 
-public class EstimateLogIn {
+public class EstimateLogIn extends ConnectDatabase{
 
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://localhost/systemdb1";
-
-	static final String USER = "root";
-	static final String PASS = "";
-	
 	public static void main(String[] args) {
-	   Connection conn = null;
-	   Statement stmt = null;
-	   
-	   try{    
-	      Class.forName("com.mysql.jdbc.Driver");
-	      
-	      System.out.println("Connecting to a selected database...");
-	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-	      System.out.println("Connected database successfully...");
-	      
-	      Scanner scan = new Scanner(System.in);
-	      System.out.println("Username: ");
-	      String InputUserName = scan.nextLine();
-	      System.out.println("Password: ");
-	      String InputPassword = scan.nextLine();
+		Connection conn = getConnection();
+	    Statement stmt = null;
+	   	      
+      try {
+    	  Scanner scan = new Scanner(System.in);
+		  System.out.println("Username: ");
+		  String InputUserName = scan.nextLine();
+		  System.out.println("Password: ");
+		  String InputPassword = scan.nextLine();
 		
-	      stmt = conn.createStatement();  
-	      String sql = "SELECT * FROM useraccount " +
-                   "WHERE UserName = '" + InputUserName + "' AND UserPassword = '" + InputPassword +"'";
-	      ResultSet rs = stmt.executeQuery(sql);
-	    
-	      if(rs.next()) {
-	    	System.out.println("Welcome User!");
+		  stmt = conn.createStatement();  
+		  String sql = "SELECT * FROM useraccount " +
+		           "WHERE UserName = '" + InputUserName + "' AND UserPassword = '" + InputPassword +"'";
+		  ResultSet rs = stmt.executeQuery(sql);
+		
+		  if(rs.next()) {
+			System.out.println("Welcome User!");
 		  } else {
-	    	System.out.println("Invalid Username and Password!");
+			System.out.println("Invalid Username and Password!");
 		  }
-
-	   }catch(SQLException se){ 
-	      se.printStackTrace();
-	   }catch(Exception e){
-	      e.printStackTrace();
-	   }finally{		   
-	      try{
-	         if(conn!=null)
-	            conn.close();
-	      }catch(SQLException se){
-	         se.printStackTrace();
-	      }
-	   }
+		  
+      } catch (SQLException e) {
+    	  e.printStackTrace();
+      }
+	      
 	   System.out.println("Goodbye!");
 	}
 }
