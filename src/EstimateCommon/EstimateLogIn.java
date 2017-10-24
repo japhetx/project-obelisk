63fc77f6fe351882a1eb420c6ce757ba76b1403d
Japhet Mert Obsioma <jpmrobsioma@gmail.com>
2017-10-25 00:10:13 +0800

    Add Connector, Trying to connect db

--- src/EstimateCommon/EstimateLogIn.java
+++ src/EstimateCommon/EstimateLogIn.java
@@ -1,7 +1,12 @@
 package EstimateCommon;
 
+import java.sql.*;
+
 public class EstimateLogIn {
 
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost/systemdb1";
+	
 	public static void main(String[] args) {
 		System.out.println("HelloWord!");
 
