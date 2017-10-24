691f63e3749fd7dbf1504c4e9f5af5f4d1136aab
Japhet Mert Obsioma <jpmrobsioma@gmail.com>
2017-10-25 00:13:27 +0800

    Connected Database Successfully

--- src/EstimateCommon/EstimateLogIn.java
+++ src/EstimateCommon/EstimateLogIn.java
@@ -6,10 +6,29 @@ public class EstimateLogIn {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://localhost/systemdb1";
+
+	static final String USER = "root";
+	static final String PASS = "";
 	
 	public static void main(String[] args) {
-		System.out.println("HelloWord!");
-
+	   Connection conn = null;
+	   try{    
+	      Class.forName("com.mysql.jdbc.Driver");
+	      System.out.println("Connecting to a selected database...");
+	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	      System.out.println("Connected database successfully...");
+	   }catch(SQLException se){ 
+	      se.printStackTrace();
+	   }catch(Exception e){
+	      e.printStackTrace();
+	   }finally{
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	         se.printStackTrace();
+	      }
+	   }
+	   System.out.println("Goodbye!");
 	}
-
 }
